a588c074e0cafdfc983dbc0a16b29381f18a93a0
李欣 <JustinDellAdam@live.com>
2017-07-26 21:01:11 +0800

    Version 0.0.2.1 Alpha

--- virtualTouchpad/Properties/AssemblyInfo.cs
+++ virtualTouchpad/Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("virtualTouchpad")]
 [assembly: AssemblyDescription("virtualTouchpad")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Lixin")]
+[assembly: AssemblyCompany("Lixin & Liuhao")]
 [assembly: AssemblyProduct("virtualTouchpad")]
-[assembly: AssemblyCopyright("Copyright © Lixin  2017")]
-[assembly: AssemblyTrademark("Lixin")]
+[assembly: AssemblyCopyright("Copyright © Lixin & Liuhao  2017")]
+[assembly: AssemblyTrademark("Lixin & Liuhao")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 会使此程序集中的类型
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.2.1")]
+[assembly: AssemblyFileVersion("0.0.2.1")]
